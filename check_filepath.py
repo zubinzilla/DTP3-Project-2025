0bddc3d280ddf7fa97934442256bb19af81c9b28
zubinzilla <jh21050@student.phs.school.nz>
2025-07-27 16:42:46 +1200

    Add files via upload

--- check_filepath.py
+++ check_filepath.py
@@ -1,35 +1,44 @@
 from customtkinter import *
-import re
+import tkinter as tk
 
-# allow user to select directory from File Explorer
-def openDir():
-    filepath = filedialog.askdirectory()
-    print(filepath)
-    subdirectories = os.path.split(filepath)
-    latest_sd = subdirectories[-1]
+# final value sent to 'file_reading.py' to be analyzed
+target_year = 0
 
-    folder_format = r"WakaNats\d{4}$"
-
-    # compares subdirectory to WakaNats conventional folder naming.
-    if re.fullmatch(folder_format, latest_sd):
-        print("This is the correct format")
-        folder_date = int(latest_sd[-4:])
-        if (folder_date >= 2017) and (folder_date <= 2040):
-            print("Folder date within valid date range (2017-2040).")
+def check_input():
+    global target_year
+    try:
+        input_year = int(entry_label.get())
+        # boundary test
+        if 2017 <= input_year <= 2040:
+            response_variable.set("Valid year")
+            target_year = input_year
         else:
-            print(f"Folder date {folder_date} not within valid date range (2017-2040).")
-    else:
-        print("This is not the correct format.")
+            response_variable.set("Entered year not between 2017 and 2040!")
 
+    # user input not a year (an integer)
+    except ValueError:
+        response_variable.set("You did not enter a year!")
 app = CTk()
 app.geometry('500x500')
+
+response_variable = tk.StringVar()
 
 set_appearance_mode("dark")
 set_default_color_theme("green")
 
-# GUI button
-select_btn = CTkButton(master=app, text='Select file', command=openDir)
-select_btn.place(relx=0.5, rely=0.45, anchor="center")
+main_label = CTkLabel(master=app, text="Analyse a Waka Ama National Game by year.", height=20, width=100, anchor="center", font=("Arial", 16))
+main_label.place(relx=0.2, rely=0.4)
+
+# Display system response to user input
+response_label = CTkLabel(master=app, textvariable=response_variable, height=20, width=255, font=("Arial", 20), bg_color="#545454"
+                          ,anchor="center")
+response_label.place(relx=0.255, rely=0.27)
+
+entry_label = CTkEntry (master=app, placeholder_text="Enter a year between 2017-2040", width=200)
+entry_label.place(relx=0.3, rely=0.5)
+
+submit_button = CTkButton(master=app, width=200, text="Submit", command=check_input)
+submit_button.place(relx=0.3, rely=0.57)
 
 app.mainloop()
 
--- file_reading.py
+++ file_reading.py
@@ -1,35 +1,45 @@
 import os
+from check_filepath import target_year
 
-folder_path = "C:/Users/jaden/Downloads/DTP3 Project/3.7B resource files/3.7B resource files/WakaNats2017"
-final_filepaths = []
-final_results = []
-no_of_files = 0
-all_records = []
 
-# collects all the file names with the
-with os.scandir(folder_path) as folder:
-    for file in folder:
-        no_of_files += 1 # increments number of files for each file scanned in directory
-        if "Final" in file.name:
-            final_filepaths.append(file.path)
+# no valid target year found (0 as default value)
+if target_year == 0:
+    print("No valid year.")
 
-# processes the data of each file
-for filepath in final_filepaths:
-    with open(filepath, "r") as file:
-        content = file.read() # would represent string data
-        file_records = content.split('\n') # splits content into array of records
-        file_records.pop(0)
-        for record in file_records:
-            if len(record) == 0:
-                file_records.pop(file_records.index(record))
-            else:
-                final_results.append(record.split(","))
+# valid target year found
+else:
+    folder_path = "C:/Users/Lenovo/Downloads/dtpAssignmentResources/3.7B resource files/3.7B resource files/WakaNats" + str(
+        target_year)
 
-print(f"Folder Opened: ")
-print(f"'{folder_path}'")
-print("Number of files: ")
-print(f"{no_of_files} files found.")
+    final_filepaths = []
+    final_results = []
+    no_of_files = 0
+    all_records = []
 
-print("Records: ")
-for record in final_results:
-    print(record)
+    # collects all the file names with the
+    with os.scandir(folder_path) as folder:
+        for file in folder:
+            no_of_files += 1 # increments number of files for each file scanned in directory
+            if "Final" in file.name:
+                final_filepaths.append(file.path)
+
+    # processes the data of each file
+    for filepath in final_filepaths:
+        with open(filepath, "r") as file:
+            content = file.read() # would represent string data
+            file_records = content.split('\n') # splits content into array of records
+            file_records.pop(0)
+            for record in file_records:
+                if len(record) == 0:
+                    file_records.pop(file_records.index(record))
+                else:
+                    final_results.append(record.split(","))
+
+    print(f"Folder Opened: ")
+    print(f"'{folder_path}'")
+    print("Number of files: ")
+    print(f"{no_of_files} files found.")
+
+    print("Records: ")
+    for record in final_results:
+        print(record)
