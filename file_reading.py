7ec70f65d78a682c3bdf426294ca0dcf17eb64b8
zubinzilla <jh21050@student.phs.school.nz>
2025-07-29 12:26:32 +1200

    Add files via upload

--- file_reading.py
+++ file_reading.py
@@ -11,35 +11,40 @@ else:
     folder_path = "C:/Users/Lenovo/Downloads/dtpAssignmentResources/3.7B resource files/3.7B resource files/WakaNats" + str(
         target_year)
 
-    final_filepaths = []
-    final_results = []
-    no_of_files = 0
-    all_records = []
+    # checks if folderpath exists
+    if os.path.isdir(folder_path):
 
-    # collects all the file names with the
-    with os.scandir(folder_path) as folder:
-        for file in folder:
-            no_of_files += 1 # increments number of files for each file scanned in directory
-            if "Final" in file.name:
-                final_filepaths.append(file.path)
+        final_filepaths = []
+        final_results = []
+        no_of_files = 0
+        all_records = []
 
-    # processes the data of each file
-    for filepath in final_filepaths:
-        with open(filepath, "r") as file:
-            content = file.read() # would represent string data
-            file_records = content.split('\n') # splits content into array of records
-            file_records.pop(0)
-            for record in file_records:
-                if len(record) == 0:
-                    file_records.pop(file_records.index(record))
-                else:
-                    final_results.append(record.split(","))
+        # collects all the file names with the
+        with os.scandir(folder_path) as folder:
+            for file in folder:
+                no_of_files += 1 # increments number of files for each file scanned in directory
+                if "Final" in file.name:
+                    final_filepaths.append(file.path)
 
-    print(f"Folder Opened: ")
-    print(f"'{folder_path}'")
-    print("Number of files: ")
-    print(f"{no_of_files} files found.")
+        # processes the data of each file
+        for filepath in final_filepaths:
+            with open(filepath, "r") as file:
+                content = file.read() # would represent string data
+                file_records = content.split('\n') # splits content into array of records
+                file_records.pop(0)
+                for record in file_records:
+                    if len(record) == 0:
+                        file_records.pop(file_records.index(record))
+                    else:
+                        final_results.append(record.split(","))
 
-    print("Records: ")
-    for record in final_results:
-        print(record)
+        print(f"Folder Opened: ")
+        print(f"'{folder_path}'")
+        print("Number of files: ")
+        print(f"{no_of_files} files found.")
+
+        print("Records: ")
+        for record in final_results:
+            print(record)
+    else:
+        print("No corresponding folder exists for that year.")
